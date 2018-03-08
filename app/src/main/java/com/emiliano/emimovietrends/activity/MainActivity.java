db889e0512ccb8a37a4a6970965199aafe31e4cc
Emiliano <tenlion@hotmail.com>
2018-03-08 01:29:49 -0300

    Add progress bar

--- app/src/main/java/com/emiliano/emimovietrends/activity/MainActivity.java
+++ app/src/main/java/com/emiliano/emimovietrends/activity/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.emiliano.emimovietrends.R;
@@ -13,20 +15,26 @@ import com.emiliano.emimovietrends.network.EmiHTTPClient;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity implements EmiHTTPClient.HttpPopularMoviesListener {
 
+    @BindView(R.id.main_activity_recycler_view)
     RecyclerView mRecyclerView;
+    @BindView(R.id.main_activity_progress_bar)
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         EmiHTTPClient.getInstance().getTrendingMovies(this, 1);
         setupAdapter();
     }
 
     private void setupAdapter() {
-        mRecyclerView = findViewById(R.id.main_activity_recycler_view);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
     }
@@ -35,10 +43,13 @@ public class MainActivity extends AppCompatActivity implements EmiHTTPClient.Htt
     public void onPopularMoviesRequestSuccess(ArrayList<Movie> movies) {
         mRecyclerView.setAdapter(new MainMoviesAdapter(this, movies));
         Toast.makeText(this, getString(R.string.success_loading_movies), Toast.LENGTH_SHORT).show();
+        mRecyclerView.setVisibility(View.VISIBLE);
+        mProgressBar.setVisibility(View.GONE);
     }
 
     @Override
     public void onPopularMoviesRequestFailed() {
         Toast.makeText(this, getString(R.string.error_loading_movies), Toast.LENGTH_SHORT).show();
+        mProgressBar.setVisibility(View.GONE);
     }
 }
